196a89e46c28be81e60327895a5f38308cb8c61c
Ravi Raina <ravi.raina@brentwood.ca>
2020-10-01 21:49:17 -0400

    Update json_to_django.py

--- JSONDjango/json_to_django.py
+++ JSONDjango/json_to_django.py
@@ -1,15 +1,19 @@
+# Script to create a 
+
 import json
 import copy
 
-with open('mcgill_grades.json') as dataf, open('output.json', 'w') as out:
+print("Enter JSON file (Make sure script is located in same folder)")
+filename = input()
+
+with open(filename) as dataf, open('output.json', 'w') as out:
     data = json.load(dataf)
     newdata = []
     for i, block in enumerate(data):
-        new = dict(model="website.Course", pk=(i+1))
-        new['fields'] = dict(average=block['average'],
-                             course=block['course'],
-                             num_average=block['num_average'],
-                             num_credit=block['num_credit'],
-                             term=block['term'])
+        new = dict(model="/PATH/TO/MODEL", pk=(i+1))
+        
+        # enter JSON fields in dict as follows: field_name=block['filed_name']
+        new['fields'] = dict(EXAMPLE1=block['EXAMPLE1'],
+                             EXAMPLE2=block['EXAMPLE2'])
         newdata.append(copy.deepcopy(new))
     json.dump(newdata, out, indent=2)
